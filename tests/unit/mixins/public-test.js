0a8207cee48891c7ed382743454c5553253f630d
DenQ <denquick@gmail.com>
2015-09-27 21:18:06 +0300

    v0.0.2

--- tests/unit/mixins/public-test.js
+++ tests/unit/mixins/public-test.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import PublicMixin from 'ember-public-mixin/mixins/public';
+import PublicMixin from '../../../mixins/public';
 import { module, test } from 'qunit';
 
 module('Unit | Mixin | public');
